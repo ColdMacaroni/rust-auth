aff025ac70560bf99b62a02c275729ed236bc105
lily <babalark@proton.me>
2024-02-09 00:21:46 +1300

    Remove reactivity from homepage
    This component should show up when the user successfully logs in

--- src/app.rs
+++ src/app.rs
@@ -37,12 +37,8 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1>"Welcome user!"</h1>
+        <p>You are logged in!</p>
     }
 }
